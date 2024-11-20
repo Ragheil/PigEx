f43b009546fb6890f32260a29c8e9ddcb4be8936
GadielDaveTagam <tagam.gadieldave@gmail.com>
2024-11-20 10:28:44 +0800

    Updated UI : Welcome Screen

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -5,14 +5,19 @@ import { LinearGradient } from 'expo-linear-gradient'; // Import LinearGradient
 export default function WelcomeScreen({ onStart }) {
   return (
     <LinearGradient
-      colors={['#FDE9EA', '#869F77', '#588061']} // Gradient colors
+      // colors={['#FDE9EA', '#869F77', '#588061']} // Gradient colors
       // colors={['#F5F5F5', '#F5F5F5']}
+      colors={['#576b4c', '#869F77', '#F5F5F5']} // Gradient colors
+      start={{ x: 0, y: 0 }} // Start at the top-left corner
+      end={{ x: 0, y: 1 }}   // End at the bottom-right corner
+      locations={[0.2, 0.4, 0.6]} // Adjust the gradient stop points
       style={styles.gradient}
     >
       <View style={styles.container}>
         {/* <Text style={styles.greeting}>Hello!</Text> */}
         {/* <Text style={styles.appName}>PigEx</Text> */}
         <View style={styles.piglogobox}>
+            {/* <Image source={require('../assets/images/logoshadow.png')} style={styles.pigLogoshadow}/> */}
             <Image source={require('../assets/images/LOGO.png')} style={styles.pigLogo} />
           </View>
         <Text style={styles.title}>Welcome to PigEx!</Text>
@@ -43,54 +48,64 @@ const styles = StyleSheet.create({
     color: '#000',
   },
   title: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: '#000',
-    letterSpacing: 1.5,
+    marginTop: 100,
+    fontSize: 40,
+    fontWeight: '500',
+    color: '#161c12',
+    letterSpacing: 1.2,
   },
   appName: {
     fontSize: 60,
     fontWeight: 'bold',
     color: '#060606',
-    marginTop: 80,
+    marginTop: 60,
     marginBottom: 200,
   },
   piglogobox: {
     // backgroundColor: 'lightblue',
-    // marginTop: 50,
-    marginBottom: 100,
+    // marginTop: 200,
+    // marginBottom: 50,
     marginVertical: 5,
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
     
   },
-  pigLogo: {
+  pigLogoshadow: {
     // backgroundColor: 'lightgreen',
     height: '35%',
     width: 230,
     resizeMode: 'contain',
-
+    zIndex: 2,
+  },
+  pigLogo: {
+    // backgroundColor: 'lightgreen',
+    height: '50%',
+    width: 300,
+    resizeMode: 'contain',
+    zIndex: 1,
   },
   description: {
     // backgroundColor: 'lightblue',
     textAlign: 'center',
-    color: '#060606',
-    fontWeight: '450',
-    marginBottom: 60,
-    paddingHorizontal: 30,
+    color: '#060606A1',
+    fontWeight: '500',
+    fontSize: 17,
+    // marginBottom: 60,
+    paddingHorizontal: 20,
     marginTop: 5,
-    lineHeight: 18,
+    lineHeight: 22,
   },
   startButton: {
     backgroundColor: '#566F48',
-    paddingVertical: 15,
-    paddingHorizontal: 100,
+    paddingVertical: 17,
+    paddingHorizontal: 135,
     borderRadius: 8,
-    marginTop: 120,
+    marginTop: 110,
   },
   startButtonText: {
     color: '#fff',
     fontWeight: 'bold',
+    fontSize: 20,
   },
 });
